6080adc2d18c9c67b501b6599c380644a1706164
Rei Sugiura <reibomaru@g.ecc.u-tokyo.ac.jp>
2020-05-29 14:16:47 +0900

    aboutページでのヘッダーの修正

--- handlers/user_about.go
+++ handlers/user_about.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"code-database/structs"
 	"html/template"
 	"log"
 	"net/http"
@@ -13,7 +14,11 @@ func AboutHandler(w http.ResponseWriter, r *http.Request, auth bool) {
 		header.IsLogin = true
 	}
 	t := template.Must(template.ParseFiles("template/user_about.html", "template/_header.html", "template/_footer.html"))
-	if err := t.Execute(w, nil); err != nil {
+	if err := t.Execute(w, struct {
+		Header structs.Header
+	}{
+		Header: header,
+	}); err != nil {
 		log.Print(err)
 		StatusInternalServerError(w, r, auth)
 		return

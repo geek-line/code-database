ce69916ca5b35f1b8b55561aa7da8293681bd54f
Rei Sugiura <reibomaru@gmail.com>
2023-10-30 11:54:59 +0900

    style: formatted models/knowledges.go

--- backend/models/knowledges.go
+++ backend/models/knowledges.go
@@ -10,7 +10,7 @@ import (
 	"code-database/structs"
 )
 
-//DeleteKnowledge 指定されたidのknowledgeを削除する
+// DeleteKnowledge 指定されたidのknowledgeを削除する
 func DeleteKnowledge(id int) error {
 	db, err := sql.Open("mysql", config.SQLEnv)
 	defer db.Close()
@@ -19,7 +19,7 @@ func DeleteKnowledge(id int) error {
 	return err
 }
 
-//GetKnowledge 指定されたidのknowledgeを取得する
+// GetKnowledge 指定されたidのknowledgeを取得する
 func GetKnowledge(id int) (structs.Knowledge, error) {
 	db, err := sql.Open("mysql", config.SQLEnv)
 	defer db.Close()
@@ -28,7 +28,7 @@ func GetKnowledge(id int) (structs.Knowledge, error) {
 	return knowledge, err
 }
 
-//GetKnowledgePublished 指定されたidで公開されているknowledgeを取得する
+// GetKnowledgePublished 指定されたidで公開されているknowledgeを取得する
 func GetKnowledgePublished(id int) (structs.Knowledge, error) {
 	db, err := sql.Open("mysql", config.SQLEnv)
 	defer db.Close()
@@ -37,7 +37,7 @@ func GetKnowledgePublished(id int) (structs.Knowledge, error) {
 	return knowledge, err
 }
 
-//GetAllKnowledges 全てのknowledgeを取得する
+// GetAllKnowledges 全てのknowledgeを取得する
 func GetAllKnowledges() ([]structs.Knowledge, error) {
 	db, err := sql.Open("mysql", config.SQLEnv)
 	defer db.Close()
@@ -52,7 +52,7 @@ func GetAllKnowledges() ([]structs.Knowledge, error) {
 	return knowledges, err
 }
 
-//GetAllPublishedKnowledges 公開されているknowledgeを全て取得する
+// GetAllPublishedKnowledges 公開されているknowledgeを全て取得する
 func GetAllPublishedKnowledges() ([]structs.Knowledge, error) {
 	db, err := sql.Open("mysql", config.SQLEnv)
 	defer db.Close()
@@ -67,7 +67,7 @@ func GetAllPublishedKnowledges() ([]structs.Knowledge, error) {
 	return knowledges, err
 }
 
-//PostKnowledge knowledgeを新規作成して作成したknowledgeのIDを取得する
+// PostKnowledge knowledgeを新規作成して作成したknowledgeのIDを取得する
 func PostKnowledge(title string, content string, rowContent string, createdAt time.Time, updatedAt time.Time, eyecatchSrc string, category string) (int64, error) {
 	db, err := sql.Open("mysql", config.SQLEnv)
 	defer db.Close()
@@ -78,7 +78,7 @@ func PostKnowledge(title string, content string, rowContent string, createdAt ti
 	return knowledgeID, err
 }
 
-//UpdateKnowledge 指定したidのknowledgeを更新する
+// UpdateKnowledge 指定したidのknowledgeを更新する
 func UpdateKnowledge(knowledgeID int, title string, content string, rowContent string, updatedAt time.Time, eyecatchSrc string, category string) error {
 	db, err := sql.Open("mysql", config.SQLEnv)
 	defer db.Close()
@@ -87,7 +87,7 @@ func UpdateKnowledge(knowledgeID int, title string, content string, rowContent s
 	return err
 }
 
-//IncrementLikes 指定されたidのlikesを増やす
+// IncrementLikes 指定されたidのlikesを増やす
 func IncrementLikes(id int) error {
 	db, err := sql.Open("mysql", config.SQLEnv)
 	defer db.Close()
@@ -96,7 +96,7 @@ func IncrementLikes(id int) error {
 	return err
 }
 
-//DecrementLikes 指定されたidのlikesを減らす
+// DecrementLikes 指定されたidのlikesを減らす
 func DecrementLikes(id int) error {
 	db, err := sql.Open("mysql", config.SQLEnv)
 	defer db.Close()
@@ -105,7 +105,7 @@ func DecrementLikes(id int) error {
 	return err
 }
 
-//GetNumOfKnowledges knowledgeの数を取得する
+// GetNumOfKnowledges knowledgeの数を取得する
 func GetNumOfKnowledges() (float64, error) {
 	db, err := sql.Open("mysql", config.SQLEnv)
 	defer db.Close()
@@ -114,7 +114,7 @@ func GetNumOfKnowledges() (float64, error) {
 	return numOfKnowledges, err
 }
 
-//GetNumOfKnowledgesFilteredByTagID 指定されたtag_idに該当するknowledgeの数を取得する
+// GetNumOfKnowledgesFilteredByTagID 指定されたtag_idに該当するknowledgeの数を取得する
 func GetNumOfKnowledgesFilteredByTagID(id int) (float64, error) {
 	db, err := sql.Open("mysql", config.SQLEnv)
 	defer db.Close()
@@ -123,7 +123,7 @@ func GetNumOfKnowledgesFilteredByTagID(id int) (float64, error) {
 	return numOfKnowledges, err
 }
 
-//GetNumOfCategoriesFilteredByCategoryID 指定されたidのカテゴリに含まれる記事の数を取得する
+// GetNumOfCategoriesFilteredByCategoryID 指定されたidのカテゴリに含まれる記事の数を取得する
 func GetNumOfCategoriesFilteredByCategoryID(id int) (float64, error) {
 	db, err := sql.Open("mysql", config.SQLEnv)
 	defer db.Close()
@@ -132,7 +132,7 @@ func GetNumOfCategoriesFilteredByCategoryID(id int) (float64, error) {
 	return numOfKnowledges, err
 }
 
-//GetNumOfKnowledgesHitByQuery 指定されたクエリの配列がコンテンツに含まれるナレッジの数を返す
+// GetNumOfKnowledgesHitByQuery 指定されたクエリの配列がコンテンツに含まれるナレッジの数を返す
 func GetNumOfKnowledgesHitByQuery(queryKeys string) (float64, error) {
 	db, err := sql.Open("mysql", config.SQLEnv)
 	defer db.Close()
@@ -142,7 +142,7 @@ func GetNumOfKnowledgesHitByQuery(queryKeys string) (float64, error) {
 	return numOfKnowledges, err
 }
 
-//Get20SortedElems 指定のsortKeyでソートされた20のknowledgeの要素を取得する
+// Get20SortedElems 指定のsortKeyでソートされた20のknowledgeの要素を取得する
 func Get20SortedElems(sortKey string, startIndex int, length int) ([]structs.IndexElem, error) {
 	db, err := sql.Open("mysql", config.SQLEnv)
 	defer db.Close()
@@ -169,7 +169,7 @@ func Get20SortedElems(sortKey string, startIndex int, length int) ([]structs.Ind
 	return indexElems, err
 }
 
-//GetRecomendedElems 任意の配列の要素の該当する記事を恣意的に取得する関数
+// GetRecomendedElems 任意の配列の要素の該当する記事を恣意的に取得する関数
 func GetRecomendedElems(indexes []string) ([]structs.IndexElem, error) {
 	intext := strings.Join(indexes, ",")
 	qtext := fmt.Sprintf("SELECT id, title, updated_at, likes, eyecatch_src FROM knowledges WHERE is_published = true AND id IN(%s)", intext)
@@ -197,7 +197,7 @@ func GetRecomendedElems(indexes []string) ([]structs.IndexElem, error) {
 	return indexElems, err
 }
 
-//Get20SortedElemFilteredTagID 指定のsortKeyでソートされ、指定のtagIdでフィルターされた20のknowledgeの要素を取得する
+// Get20SortedElemFilteredTagID 指定のsortKeyでソートされ、指定のtagIdでフィルターされた20のknowledgeの要素を取得する
 func Get20SortedElemFilteredTagID(sortKey string, tagID int, startIndex int, length int) ([]structs.IndexElem, string, error) {
 	db, err := sql.Open("mysql", config.SQLEnv)
 	defer db.Close()
@@ -227,7 +227,7 @@ func Get20SortedElemFilteredTagID(sortKey string, tagID int, startIndex int, len
 	return indexElems, tagName, err
 }
 
-//Get20SortedElemFilteredByCategoryID 指定のsortKeyでソートされ、指定のcategoryIdでフィルターされた20のknowledgeの要素を取得する
+// Get20SortedElemFilteredByCategoryID 指定のsortKeyでソートされ、指定のcategoryIdでフィルターされた20のknowledgeの要素を取得する
 func Get20SortedElemFilteredByCategoryID(sortKey string, categoryID int, startIndex int, length int) ([]structs.IndexElem, structs.Category, error) {
 	db, err := sql.Open("mysql", config.SQLEnv)
 	defer db.Close()
@@ -259,7 +259,7 @@ func Get20SortedElemFilteredByCategoryID(sortKey string, categoryID int, startIn
 	return indexElems, category, err
 }
 
-//Get20SortedElemHitByQuery 指定されたクエリを含むコンテンツにヒットしたナレッジのなかで上位20を返す
+// Get20SortedElemHitByQuery 指定されたクエリを含むコンテンツにヒットしたナレッジのなかで上位20を返す
 func Get20SortedElemHitByQuery(sortKey string, queryKeys string, startIndex int, length int) ([]structs.IndexElem, error) {
 	db, err := sql.Open("mysql", config.SQLEnv)
 	defer db.Close()
@@ -289,7 +289,7 @@ func Get20SortedElemHitByQuery(sortKey string, queryKeys string, startIndex int,
 	return indexElems, err
 }
 
-//SetPublicKnowledge 指定されたidのナレッジを公開する
+// SetPublicKnowledge 指定されたidのナレッジを公開する
 func SetPublicKnowledge(id int) error {
 	db, err := sql.Open("mysql", config.SQLEnv)
 	defer db.Close()
@@ -298,7 +298,7 @@ func SetPublicKnowledge(id int) error {
 	return err
 }
 
-//SetPrivateKnowledge 指定されたidのナレッジを非公開にする
+// SetPrivateKnowledge 指定されたidのナレッジを非公開にする
 func SetPrivateKnowledge(id int) error {
 	db, err := sql.Open("mysql", config.SQLEnv)
 	defer db.Close()

88aa4e3739a1eb41c0c272f6e2d257681cd43e24
Rei Sugiura <reibomaru@gmail.com>
2024-02-17 12:33:50 +0900

    refactor: updated bucket settings

--- frontend/src/helpers/aws_init.ts
+++ frontend/src/helpers/aws_init.ts
@@ -1,7 +1,7 @@
 import AWS from 'aws-sdk'
-export const albumBucketName = 'code-database-images-ver2'
+export const albumBucketName = 'code-database-images-2024'
 const bucketRegion = 'ap-northeast-1'
-const IdentityPoolId = 'ap-northeast-1:732737cb-e31c-4318-ae61-a8bf0ab12299'
+const IdentityPoolId = 'ap-northeast-1:495e5b92-be6c-4dd2-af98-339b4ebe30af'
 
 AWS.config.update({
   region: bucketRegion,
